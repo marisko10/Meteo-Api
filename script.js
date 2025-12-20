6e2eea711bc2f86060d71170cee5ad4628db3ce9
Pedro <pedromcfreitas10@gmail.com>
2025-12-20 00:49:01 +0000

    master

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-// A tua API key
 const API_KEY = '9f42b3b7a7d547f5b29235848251212';
 
-// Elementos do HTML
-const cityInput = document.getElementById('cityInput');
-const searchBtn = document.getElementById('searchBtn');
-const result = document.getElementById('result');
+const input = document.getElementById("cityInput");
+const button = document.getElementById("searchBtn");
+const result = document.getElementById("result");
 
-// Buscar o tempo
-async function getWeather(city) {
-  result.innerHTML = '<p>A carregar...</p>';
-  
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`);
+async function weather(city) {
+  const response = await fetch (`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`);
   const data = await response.json();
   
-  const temp = Math.round(data.current.temp_c);
-  result.innerHTML = `<h2>${data.location.name}</h2><p class="temp">${temp}°C</p>`;
+  result.innerHTML =  `<h2> Weather in ${data.location.name} </h2>` +
+                      `<p> Temperature: ${data.current.temp_c}°C </p>`
 }
 
-// Clicar no botão
-searchBtn.addEventListener('click', () => {
-  getWeather(cityInput.value);
-});
 
-// Tecla Enter
-cityInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') getWeather(cityInput.value);
-});
+button.addEventListener("click", () => {
+  weather(input.value);
+})
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    weather(input.value);
+  }
+})
+
