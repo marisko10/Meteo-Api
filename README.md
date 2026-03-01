3e8d7b7dcb1f2791fecbe6ca4ce81a06a68f800b
Pedro <pedromcfreitas10@gmail.com>
2026-03-01 00:10:23 +0000

    Projeto Finalizado

---
+++ README.md
@@ -0,0 +1,25 @@
+                  Weather App
+
+Aplicação web simples que permite pesquisar o clima de qualquer cidade utilizando uma API de meteorologia.
+
+
+                  Funcionalidades
+
+- Pesquisar o clima por cidade
+- Mostrar temperatura, condição climática e localização
+- Interface simples e responsiva
+
+
+                    Tecnologias
+
+- HTML5
+- CSS3
+- JavaScript (ES6)
+- API de meteorologia
+
+
+
+                   Demo Online  
+meteo-e0fv6kjwc-pedromcfreitas10-1648s-projects.vercel.app 
+
+
---
+++ script.js
@@ -0,0 +1,27 @@
+const API_KEY = '9f42b3b7a7d547f5b29235848251212';
+
+const input = document.getElementById("cityInput");
+const button = document.getElementById("searchBtn");
+const result = document.getElementById("result");
+
+async function weather(city) {
+  const response = await fetch (`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`);
+  const data = await response.json();
+  
+  result.innerHTML =  `<h2> Weather in ${data.location.name} </h2>` +
+                      `<p> Temperature: ${data.current.temp_c}°C </p>`
+}
+
+
+button.addEventListener("click", () => {
+  weather(input.value);
+  input.value = "";
+})
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    weather(input.value);
+    input.value = "";
+  }
+})
+
